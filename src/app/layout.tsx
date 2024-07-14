794ddb3d825d1110b88afce258b11ff02081e639
abderrahmane <ilahalaha0987@gmail.com>
2024-07-14 19:56:21 +0100

    the page is responsive on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="flex flex-col justify-center  items-center py-16 bg-black w-full h-full">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col max-w-[1500px] items-center py-16 bg-black w-full h-full">
+    <main className="max-w-[1500px] flex items-center flex-col justify-center p-3 ">
       <img src="/pageTitle.png" width="900px" />
-      <h5 className="text-3xl text-white tracking-wider font-bold mt-11" >Tap on your favorite Spudz candidate to cast your vote!</h5>
+      <h5 className="text-3xl text-white tracking-wider font-bold mt-11 text-center" >Tap on your favorite Spudz candidate to cast your vote!</h5>
       <img src="/pageFlag.png" width="820px" />
       <div className="flex relative  justify-center gap-32 px-14 w-full  z-0 mt-11" >
         <div className="flex z-10 relative flex-col gap-7" >
        <img  className="w-[400px]" src="/trumpText.png" />
        <img  className="w-[400px]" src="/trumpBody.png" />
        </div>
-       <div className="z-0 absolute top-1/2 flex  justify-center left-1/2 w-[550px] -translate-x-1/2 -translate-y-1/2" >
-       <img src="/vsBg.png" className="absolute z-[1]  opacity-70  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" width="550px" />
+       <div className="z-0 absolute top-1/2 flex  justify-center left-1/2 w-7/12 -translate-x-1/2 -translate-y-1/2" >
+       <img src="/vsBg.png" className="absolute z-[1]  opacity-70  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full"  />
        <img src="/vs.png" className="z-[2] h-fit w-[320px] -translate-y-24" />
        </div>
        <div className="flex z-10 relative flex-col gap-7" >
