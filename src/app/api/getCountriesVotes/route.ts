d96694bff9d29302852f17b388c658a7443cfd87
abderrahmane <ilahalaha0987@gmail.com>
2024-07-17 18:32:47 +0100

    fixing cashing issues on api

--- src/app/api/getCountriesVotes/route.ts
+++ src/app/api/getCountriesVotes/route.ts
@@ -1,17 +1,26 @@
-import {  connectDbPromise } from "@/db/connect"
-import { CountryModal } from "@/db/modals/countryDb"
-import { addVoteValidator } from "@/utils/validators"
-import { NextRequest } from "next/server"
+import { NextRequest } from 'next/server';
+import connectDbPromise from './pathToYourDbConnection';
+import CountryModel from './pathToYourCountryModel';
 
-
-
-export const GET =  async  (req  : NextRequest)=>{
-try {
-await connectDbPromise
-const countriesVotes = await CountryModal().find()
-return new Response(JSON.stringify(countriesVotes) , {status : 200})
-}
-catch(err){
-    console.log(err)
-}
-}
+export const GET = async (req: NextRequest) => {
+  try {
+    await connectDbPromise;
+    const countriesVotes = await CountryModel.find();
+    
+    const response = new Response(JSON.stringify(countriesVotes), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store'
+      }
+    });
+    
+    return response;
+  } catch (err) {
+    console.log(err);
+    return new Response('Internal Server Error', { status: 500 });
+  }
+};
