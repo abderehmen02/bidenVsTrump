f2f7fa06d1b6ac5f2fa36e37040104d5cd119ded
abderrahmane <ilahalaha0987@gmail.com>
2024-07-17 18:34:14 +0100

    fixing api issues

--- src/app/api/getCountriesVotes/route.ts
+++ src/app/api/getCountriesVotes/route.ts
@@ -1,26 +1,27 @@
-import { NextRequest } from 'next/server';
-import connectDbPromise from './pathToYourDbConnection';
-import CountryModel from './pathToYourCountryModel';
+import {  connectDbPromise } from "@/db/connect"
+import { CountryModal } from "@/db/modals/countryDb"
+import { addVoteValidator } from "@/utils/validators"
+import { NextRequest } from "next/server"
 
-export const GET = async (req: NextRequest) => {
-  try {
-    await connectDbPromise;
-    const countriesVotes = await CountryModel.find();
-    
-    const response = new Response(JSON.stringify(countriesVotes), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-        'Surrogate-Control': 'no-store'
-      }
-    });
-    
-    return response;
-  } catch (err) {
-    console.log(err);
-    return new Response('Internal Server Error', { status: 500 });
-  }
-};
+
+
+export const GET =  async  (req  : NextRequest)=>{
+try {
+await connectDbPromise
+const countriesVotes = await CountryModal().find()
+const response = new Response(JSON.stringify(countriesVotes), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+      'Surrogate-Control': 'no-store'
+    }
+  });
+  
+  return response;}
+catch(err){
+    console.log(err)
+}
+}
