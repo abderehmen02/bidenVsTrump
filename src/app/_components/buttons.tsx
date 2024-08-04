87537a8380c6bd943ac483ba06d59faf9d0d8429
abderrahmane <ilahalaha0987@gmail.com>
2024-08-04 14:59:02 +0100

    integrating the animation for the buy now button

--- src/app/_components/buttons.tsx
+++ src/app/_components/buttons.tsx
@@ -1,8 +1,18 @@
+"use client"
+import {motion} from "framer-motion"
 
+
+export const HeaderButtonsDesktop = ()=>{
+    return       <div className="w-full flex items-center justify-between" >
+     <motion.div className="cursor-pointer z-20 relative"  whileHover={{rotate : [ 7, -7 , 5 , -5  , 3 , -3 , 1 , -1 , 0 , 0] , transition : {duration  : 1, ease: "easeIn" } }}  >   <img className="hidden lg:block" width="200px" src="/buyNow.png" style={{margin :0}} /></motion.div>
+    <img src="/pageTitle.png" width="600px" className="" style={{margin : 0}}  />
+    <img className="hidden lg:block" width="200px" src="/home.png" style={{margin :0}} />
+    </div>
+}
 
 export const PageButtons = ()=>{
-    return <div className="flex py-1 px-1 lg:hidden w-full    items-center justify-around" >
-              <img width="200px" src="/buyNow.png" style={{margin :0 }} />
+    return <div  className="flex py-1  px-1 lg:hidden w-full  relative  z-10  items-center justify-around" >
+              <img  width="200px" src="/buyNow.png" style={{margin :0 }} />
               <img width="150px" src="/home.png" style={{margin :0  }} />
     </div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { TotalVotes } from "./_components/totalVotes";
 import { BidenVotes, GreaterSign, TrumpVotes } from "./_components/candidates";
 import { connectDbPromise } from "@/db/connect";
 import { MyVotesSection } from "./_components/myVotesSection";
-import { PageButtons } from "./_components/buttons";
+import { HeaderButtonsDesktop, PageButtons } from "./_components/buttons";
 
 const env = process.env.NODE_ENV
 export default async function Home() {
@@ -22,11 +22,7 @@ export default async function Home() {
   
    return (
     <main className="lg:max-w-[1500px] max-w-full lg:max-h-screen overflow-hidden flex items-center  flex-col justify-center pt-3 lg:pt-0 px-3 pb-0 ">
-      <div className="w-full flex items-center justify-between" >
-      <img className="hidden lg:block" width="200px" src="/buyNow.png" style={{margin :0}} />
-      <img src="/pageTitle.png" width="600px" className="" style={{margin : 0}}  />
-      <img className="hidden lg:block" width="200px" src="/home.png" style={{margin :0}} />
-      </div>
+      <HeaderButtonsDesktop/>
       <PageButtons/>
 
       <h5 style={{margin : 0}} className="text-3xl text-white tracking-wider font-bold lg:mt-0 mt-11 text-center" >Tap on your favorite Spudz candidate to cast your vote!</h5>
