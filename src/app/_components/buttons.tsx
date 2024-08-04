d5f793927ab84d6b122ac22dcd245556aff00282
abderrahmane <ilahalaha0987@gmail.com>
2024-08-04 15:45:14 +0100

    modifying the rotate btn animation

--- src/app/_components/buttons.tsx
+++ src/app/_components/buttons.tsx
@@ -1,10 +1,18 @@
 "use client"
-import {motion} from "framer-motion"
+import {motion, useAnimation} from "framer-motion"
 
 
 export const HeaderButtonsDesktop = ()=>{
+    const rotateBtn = useAnimation()
+
+const onHoverStart = ()=>{
+    rotateBtn.start({
+        rotate : [ 7, -7 , 5 , -5  , 3 , -3 , 1 , -1 , 0 , 0] , transition : {duration  : 1, ease: "easeIn" } 
+    })
+}
+
     return       <div className="w-full flex items-center justify-between" >
-     <motion.div className="cursor-pointer z-20 relative"  whileHover={{rotate : [ 7, -7 , 5 , -5  , 3 , -3 , 1 , -1 , 0 , 0] , transition : {duration  : 1, ease: "easeIn" } }}  >   <img className="hidden lg:block" width="200px" src="/buyNow.png" style={{margin :0}} /></motion.div>
+     <motion.div animate={rotateBtn} onHoverStart={onHoverStart} className="cursor-pointer z-20 relative"   >   <img className="hidden lg:block" width="200px" src="/buyNow.png" style={{margin :0}} /></motion.div>
     <img src="/pageTitle.png" width="600px" className="" style={{margin : 0}}  />
     <img className="hidden lg:block" width="200px" src="/home.png" style={{margin :0}} />
     </div>
