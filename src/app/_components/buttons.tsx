b923677fe524b5d7c939ce945ef877928e5202be
abderrahmane <ilahalaha0987@gmail.com>
2024-08-16 17:45:49 +0100

    integrating the buy now link

--- src/app/_components/buttons.tsx
+++ src/app/_components/buttons.tsx
@@ -15,7 +15,7 @@ const onHoverStart = ()=>{
 
 
     return       <div style={{margin : 0}} className="w-full flex  relative items-center justify-between" >
-     <motion.div animate={rotateBtn} onHoverStart={onHoverStart} className="cursor-pointer hidden lg:block z-0 relative"   >   <img className="hidden lg:block" width="200px" src="/buyNow.png" style={{margin :0}} /></motion.div>
+     <motion.a target="_blank" href="https://spudz-presale.netlify.app/" animate={rotateBtn} onHoverStart={onHoverStart} className="cursor-pointer hidden lg:block z-0 relative"   >   <img className="hidden lg:block" width="200px" src="/buyNow.png" style={{margin :0}} /></motion.a>
     <img src="/pageTitle.png"  className="w-[400px] lg:w-[600px]" style={{margin : 0}}  />
     <div className="flex gap-5" >
     <motion.button whileHover={{scale : 1.1}} className="hidden relative  lg:block px-6 py-2  z-0 text-xl bg-green-700 text-white font-bold border-4 rounded-xl border-white" >ABOUT $SPUDZ</motion.button>
@@ -29,10 +29,11 @@ const onHoverStart = ()=>{
 }
 
 export const PageButtons = ()=>{
+    
     return <div style={{margin :0}} className="flex  py-2 lg:py-3    px-1 lg:hidden w-full  relative  z-0  items-center justify-around" >
             <img className="cursor-pointer"  width="25px" src="/telegram.png"   />
 
-              <img  className="w-[130px] relative z-0  lg:w-[200px]" src="/buyNow.png" style={{margin :0 }} />
+               <a href="https://spudz-presale.netlify.app/" > <img   className="w-[130px] relative z-0  lg:w-[200px]" src="/buyNow.png" style={{margin :0 }} /></a>
               <motion.button whileHover={{scale : 1.1}} className="relative z-20 lg:block lg:px-6 px-2 py-1 lg:py-2  lg:text-xl bg-green-700 text-white font-bold border-2 rounded-xl border-white" >ABOUT $SPUDZ</motion.button>
               <div className="gap-1 flex" >
     <img className="cursor-pointer"  width="25px" src="/x.webp"  />
