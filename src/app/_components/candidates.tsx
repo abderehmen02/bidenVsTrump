50f7712a4cfa6a4dce7ed6ef07b389ca9de99f28
abderrahmane <ilahalaha0987@gmail.com>
2024-08-31 19:10:37 +0100

    fixing the greater than sign

--- src/app/_components/candidates.tsx
+++ src/app/_components/candidates.tsx
@@ -1,104 +1,135 @@
-"use client"
-import { useCountriesVotesStore } from "@/store/countriesDb"
+"use client";
+import { useCountriesVotesStore } from "@/store/countriesDb";
 import { cn } from "@/utils/tailwind";
-import {motion, useAnimation} from "framer-motion"
+import { motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
 
+export const TrumpVotes = () => {
+  const { countriesVotes } = useCountriesVotesStore();
+  let racio: number | undefined;
+  let totalTrumVotes: number | undefined;
+  let totalVotes: number | undefined;
+  for (let i = 0; i < countriesVotes.length; i++) {
+    totalVotes =
+      (totalVotes || 0) + (countriesVotes[i].biden + countriesVotes[i].trump);
+  }
 
+  for (let i = 0; i < countriesVotes.length; i++) {
+    totalTrumVotes = (totalTrumVotes || 0) + countriesVotes[i].trump;
+  }
 
-export const TrumpVotes = ()=>{
-    const {countriesVotes} = useCountriesVotesStore()
-    let racio : number | undefined ;
-    let totalTrumVotes : number | undefined ;
-    let totalVotes : number | undefined  ;
-    for(let i = 0 ; i< countriesVotes.length ; i++){
-        totalVotes = (totalVotes || 0) + (countriesVotes[i].biden + countriesVotes[i].trump)
-    }
+  if (totalTrumVotes && totalVotes) {
+    racio = (totalTrumVotes / totalVotes) * 100;
+  }
 
-     for(let i = 0 ; i < countriesVotes.length ; i++){
-        totalTrumVotes = (totalTrumVotes || 0 ) +  countriesVotes[i].trump
-     }
-
-     
-
-     if(totalTrumVotes && totalVotes){
-       racio = (totalTrumVotes / totalVotes) * 100
-     }
-
-
-
-    return       <div className="flex  flex-col lg:flex-row xl:flex-col lg:gap-2 gap-2 items-center" >
-    <h4 style={{margin : 0}} className="text-xl lg:text-4xl text-white tracking-wider font-bold mt-11 text-center  m-0 leading-none" >Trump Spudz</h4>
-    {totalTrumVotes ? <h4 style={{margin : 0}} className="H4" >{totalTrumVotes.toLocaleString()}</h4> : <div className="skeleton w-24 h-9" ></div> }
-    {racio ?  <button style={{margin : 0}} className="bg-red-500 rounded-xl H4 p-2 lg:p-4" >{racio.toFixed(1)}%</button> : <div className="skeleton w-32 h-20" ></div> }
+  return (
+    <div className="flex  flex-col lg:flex-row xl:flex-col lg:gap-2 gap-2 items-center">
+      <h4
+        style={{ margin: 0 }}
+        className="text-xl lg:text-4xl text-white tracking-wider font-bold mt-11 text-center  m-0 leading-none"
+      >
+        Trump Spudz
+      </h4>
+      {totalTrumVotes ? (
+        <h4 style={{ margin: 0 }} className="H4">
+          {totalTrumVotes.toLocaleString()}
+        </h4>
+      ) : (
+        <div className="skeleton w-24 h-9"></div>
+      )}
+      {racio ? (
+        <button
+          style={{ margin: 0 }}
+          className="bg-red-500 rounded-xl H4 p-2 lg:p-4"
+        >
+          {racio.toFixed(1)}%
+        </button>
+      ) : (
+        <div className="skeleton w-32 h-20"></div>
+      )}
     </div>
-}
+  );
+};
 
+export const BidenVotes = () => {
+  const { countriesVotes } = useCountriesVotesStore();
 
-export const BidenVotes = ()=>{
-    const {countriesVotes} = useCountriesVotesStore()
+  let totalBidenVotes: number | undefined;
+  let racio: number | undefined;
+  let totalVotes: number | undefined;
+  for (let i = 0; i < countriesVotes.length; i++) {
+    totalVotes =
+      (totalVotes || 0) + (countriesVotes[i].biden + countriesVotes[i].trump);
+  }
 
-    let totalBidenVotes : number | undefined ; ;
-    let racio   : number | undefined ;
-    let totalVotes : number | undefined  ;
-    for(let i = 0 ; i< countriesVotes.length ; i++){
-        totalVotes = (totalVotes || 0) + (countriesVotes[i].biden + countriesVotes[i].trump)
-    }
+  for (let i = 0; i < countriesVotes.length; i++) {
+    totalBidenVotes = (totalBidenVotes || 0) + countriesVotes[i].biden;
+  }
 
-     for(let i = 0 ; i < countriesVotes.length ; i++){
-        totalBidenVotes = (totalBidenVotes || 0 ) + countriesVotes[i].biden
-     }
+  if (totalBidenVotes && totalVotes) {
+    racio = (totalBidenVotes / totalVotes) * 100;
+  }
 
+  return (
+    <div className="flex flex-col lg:flex-row xl:flex-col  lg:gap-2 gap-2 items-center">
+      <h4 style={{ margin: 0 }} className="H4">
+        Haris Spudz
+      </h4>
+      {totalBidenVotes ? (
+        <h4 style={{ margin: 0 }} className="H4">
+          {totalBidenVotes.toLocaleString()}
+        </h4>
+      ) : (
+        <div className="skeleton w-24 h-9"></div>
+      )}
+      {racio ? (
+        <button
+          style={{ margin: 0 }}
+          className="bg-red-500 rounded-xl H4 p-2 lg:p-4"
+        >
+          {racio.toFixed(1)}%
+        </button>
+      ) : (
+        <div className="skeleton w-32 h-20"></div>
+      )}
+    </div>
+  );
+};
 
-     
-     if(totalBidenVotes && totalVotes){
-        racio = (totalBidenVotes / totalVotes) * 100
-      }
- 
+export const GreaterSign = () => {
+  const { countriesVotes } = useCountriesVotesStore();
+  const changeWinnerAnimation = useAnimation();
 
+  let totalBidenVotes: number | undefined;
+  let totalTrumpVotes: number | undefined;
+  let isTrumpWinner: boolean | undefined = undefined;
 
+  for (let i = 0; i < countriesVotes.length; i++) {
+    totalBidenVotes = (totalBidenVotes || 0) + countriesVotes[i].biden;
+    totalTrumpVotes = (totalTrumpVotes || 0) + countriesVotes[i].trump;
+  }
 
-    return        <div className="flex flex-col lg:flex-row xl:flex-col  lg:gap-2 gap-2 items-center" >
-    <h4 style={{margin : 0}} className="H4" >Haris Spudz</h4>
-    {totalBidenVotes ?  <h4 style={{margin : 0}} className="H4" >{totalBidenVotes.toLocaleString()}</h4> : <div className="skeleton w-24 h-9" ></div> }
-    {racio ?  <button style={{margin : 0}} className="bg-red-500 rounded-xl H4 p-2 lg:p-4" >{racio.toFixed(1)}%</button> : <div className="skeleton w-32 h-20" ></div>  }
-   </div>
-}
+  if (totalBidenVotes && totalTrumpVotes) {
+    isTrumpWinner = totalTrumpVotes > totalBidenVotes;
+  }
 
+  useEffect(() => {
+    changeWinnerAnimation.start({
+      rotate: isTrumpWinner ? 180 : 0,
+      transition: { duration: 0.5 },
+    });
+  }, [isTrumpWinner]);
 
+  if (isTrumpWinner === undefined) {
+    return <div className="skeleton w-32 h-24 "></div>;
+  }
 
-export const GreaterSign = ()=>{
-const {countriesVotes} = useCountriesVotesStore()
-const changeWinnerAnimation = useAnimation()
-
-let totalBidenVotes : number | undefined ; ;
-let totalTrumpVotes : number | undefined ; ;
-let isTrumpWinner : boolean | undefined = undefined ; 
-
-for(let i = 0 ; i < countriesVotes.length ; i++){
-   totalBidenVotes = (totalBidenVotes || 0 ) + countriesVotes[i].biden
-   totalTrumpVotes  = (totalTrumpVotes || 0 ) + countriesVotes[i].trump
-}
-
-if(totalBidenVotes && totalTrumpVotes) {
-    isTrumpWinner = totalTrumpVotes > totalBidenVotes
-}
- 
-
-useEffect(()=>{
-    changeWinnerAnimation.start(
-        {rotate : isTrumpWinner ? 0 : 180 , transition : {duration : 0.5} } 
-    )
-    } , [isTrumpWinner] )
-
-
-if(isTrumpWinner === undefined){
-    return <div className="skeleton w-32 h-24 " ></div>
-}
-
-
-
-
-    return        <motion.img animate={changeWinnerAnimation} initial={{rotate : isTrumpWinner ? 0 : 180}} src="/greaterSign.png" className={cn("w-16 lg:w-24" )} ></motion.img>
-
-}
+  return (
+    <motion.img
+      animate={changeWinnerAnimation}
+      initial={{ rotate: isTrumpWinner ? 180 : 0 }}
+      src="/greaterSign.png"
+      className={cn("w-16 lg:w-24")}
+    ></motion.img>
+  );
+};
